fccddf4913e084b884c628c4ec9b7f4b568bab7a
Andrew Surya <andrewsuryaaccforprogramming@gmail.com>
2024-04-04 21:01:15 +0800

    operating system until step 3

--- Sophomore/Spring 下/Operating System/Programming Assigment/Steps.md
+++ Sophomore/Spring 下/Operating System/Programming Assigment/Steps.md
@@ -1,5 +1,7 @@
 1. making my shell can exit 
 
+
+Making fork in my code, so that it can be active 
 2. making my shell can run ls ✔ 
     -test running in minix ✔
 
--- Sophomore/Spring 下/Operating System/Programming Assigment/export_to_minix/test1.c
+++ Sophomore/Spring 下/Operating System/Programming Assigment/export_to_minix/test1.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> // execvp 
+#include <unistd.h> // execvp()
+#include <sys/types.h> // waitpid()
+#include <sys/wait.h> // waitpid()
+
+
 
 #define MAX_LINE_LENGTH 256
 #define MAX_ARGUMENT 50
 
 // 
-void process_input(char* buffer, char* array[], int *argumentCount)
+void process_input(char* buffer, char* array[])
 {
     char* token; 
     int i = 0;
+
+    buffer[strcspn(buffer, "\n")] = 0; // remove newline 
 
     token = strtok(buffer, " ");
 
@@ -18,38 +24,51 @@ void process_input(char* buffer, char* array[], int *argumentCount)
     {
         array[i++] = token;
         token = strtok(NULL, " ");
-        printf("token%s\n", token);
     }
+
     array[i] = NULL;
-
-    *argumentCount = i;
-
-    for (int j = 0; j < i; j++)
-    {
-        printf("in array %s\n", array[j]);
-    }
-
-    printf("\n");
 }
 
 // Executing the input
-void execute_input(char* array[], int *argumentCount)
+void execute_input(char* array[])
+{
+    pid_t pid = fork(); // Create a new process
+
+    if (pid < 0)
+    {
+        // Fork Failed
+        perror("fork");
+    }
+    else if (pid > 0)
+    {
+        // Parent process
+        int status;
+        waitpid(pid, &status, 0); // Wait for the child process to finish and free the child memory, prevent zombie process
+    }
+    else
+    {
+        // Child process
+       
+        execvp(array[0], array);
+        perror("execvp");
+        exit(EXIT_FAILURE); // macro that represent failure exit, usually set to 1
+    }
+
+
+    // char *args[] = { "ls", "-l", NULL }; // Command and its arguments
+    // execvp(args[0], args); // Execute ls command
+    //perror("execvp");
+}
+
+void debug_print(char* array[])
 {
     int i = 0;
+
     while(array[i] != NULL) 
     {
-        printf("inside array%s\n", array[i]);
+        printf("inside array%sEND\n", array[i]);
         i++;
     }
-    // execvp(array[0], array);
-    // perror("execvp");
-
-
-    char *args[] = { "ls", NULL }; // Command and its arguments
-    execvp(args[0], args); // Execute ls command
-    //perror("execvp");
-
-    printf("andrew\n");
 }
 
 int main()
@@ -63,7 +82,7 @@ int main()
         // count of argument in current line 
         int argumentCount = -1; 
 
-        printf("# ");
+        printf("andrew shell# ");
         if (fgets(buffer, sizeof(buffer), stdin) != NULL) 
         {
             if (strcmp(buffer, "exit\n") == 0)
@@ -71,21 +90,16 @@ int main()
                 return 0;
             }
 
-            process_input(buffer, arguments, &argumentCount);
-            execute_input(arguments , &argumentCount);
+            process_input(buffer, arguments);
+            execute_input(arguments);
 
-            for(int i = 0; i < argumentCount; i++)
-                printf("%s", arguments[i]);
-
-
-
-            printf("%s\n", buffer);
         }
         else 
         {
             printf("%s: not found.\n", buffer);
         }
 
+        printf("\n");
     }
 
 
